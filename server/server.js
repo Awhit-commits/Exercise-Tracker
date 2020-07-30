88f3a5b171b944e6135f0d401fe062a113519d10
Awhit-commits <ndrw_whtmr@ymail.com>
2020-07-29 22:09:20 -0500

    deploying

--- server/server.js
+++ server/server.js
@@ -4,7 +4,8 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const user = require('./routes/user');
 const exercises = require('./routes/exercises')
-
+const bodyParser = require('body-parser');
+const path = require('path')
 require ('dotenv').config();
 
 //middleware
@@ -13,8 +14,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
 app.use('/users',user);
 app.use('/exercises',exercises)
+
+//
+app.use(express.static(path.join(__dirname, '../build')))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build'))
+})
+//
 let mongoDB = 'mongodb+srv://ndrwwhtmr:VaUOsbhP8iq8kqQ2@cluster0-taich.mongodb.net/test?retryWrites=true&w=majority'
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
 // connection error message
